ac08ad471e2417c36eaf9a36a4dd5143fddde0df
Emma McArdle <emmahmcardle@gmail.com>
2021-04-10 17:58:36 -0400

    Updated home page to be more responsive and reflective of the design

--- app.py
+++ app.py
@@ -19,25 +19,25 @@ mail = Mail(app)
 # app.config
 
 @app.route('/')
-@app.route('/home')
-def home():
+@app.route('/work')
+def work():
     return render_template("index.html")
 
 @app.route('/about')
 def about():
     return render_template("about.html")
 
-@app.route('/services', methods=['GET', 'POST'])
-def services():
-    if request.method == "POST":
-        name = request.form.get('name')
-        email = request.form.get('email')
-        phone = request.form.get('phone')
-        message = request.form.get('message')
-        # See if you can retitle
-        msg = Message(subject=f"Website form message from {name}", body=f"Name: {name}\nE-Mail: {email}\nPhone: {phone}\n\n\n{message}", sender="ajkworksmartcontactform@gmail.com", recipients=["coolerstuff10@yahoo.com"])
-        mail.send(msg)
-    return render_template("services.html")
+# @app.route('/products', methods=['GET', 'POST'])
+# def products():
+    # if request.method == "POST":
+    #     name = request.form.get('name')
+    #     email = request.form.get('email')
+    #     phone = request.form.get('phone')
+    #     message = request.form.get('message')
+    #     # See if you can retitle
+    #     msg = Message(subject=f"Website form message from {name}", body=f"Name: {name}\nE-Mail: {email}\nPhone: {phone}\n\n\n{message}", sender="ajkworksmartcontactform@gmail.com", recipients=["coolerstuff10@yahoo.com"])
+    #     mail.send(msg)
+    # return render_template("products.html")
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
